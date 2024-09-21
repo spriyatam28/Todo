9831b04616e60999e08ffbe79a78ee9689a238d3
spriyatam28 <priyatam777.dev@users.noreply.github.com>
2024-09-21 12:14:28 +0530

    added tasks component and favicon

--- src/pages/home/components/App.jsx
+++ src/pages/home/components/App.jsx
@@ -1,13 +1,17 @@
 import '../../../styles/App.css';
 import Header from "./Header.jsx";
+import Tasks from "./Tasks.jsx";
 
 function App() {
-  return (
-      <div>
-          <h1>Tasks</h1>
-          <Header/>
-      </div>
-  );
+    return (
+        <div>
+            <h1>Tasks</h1>
+            <main>
+                <Header/>
+                <Tasks/>
+            </main>
+        </div>
+    );
 }
 
 export default App
---
+++ src/pages/home/components/Tasks.jsx
@@ -0,0 +1,13 @@
+import '../../../styles/Tasks.css';
+
+function Tasks() {
+    return (
+        <div id="tasks">
+            <div>
+                <button>No Tasks</button>
+            </div>
+        </div>
+    );
+}
+
+export default Tasks;
